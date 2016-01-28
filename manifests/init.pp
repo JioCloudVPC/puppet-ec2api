0bd3b2b644a112a5aac6a0823b383569886929aa
vpramo <pramod.venkatesh@ril.com>
2016-01-28 14:47:47 +0530

    Added ec2api puppet

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,16 @@
 #
 # Main EC2 API class to configure the service via puppet.
 #
-class ec2api {
+class ec2api(
+  $ec2api_config = {},
+  $api_paste_ini_config ={},
+  $manage_service = true,
+) {
+
+
+  validate_hash($ec2api_config)
+  validate_hash($api_paste_ini_config)
+  validate_bool($manage_service)
 
   include ec2api::params
 
@@ -11,7 +20,6 @@ class ec2api {
     ensure => present,
     name   => $ec2api::params::package_name,
   }
-
 
   # Chanage onwer, group and permissions to config files
   file { $ec2api::params::ec2api_config:
@@ -29,5 +37,24 @@ class ec2api {
     mode    => '0644',
     require => Package['ec2api'],
   }
+  
+  $defaults = {
+    notify => Service['ec2api'],
+    require => Package['ec2api']
+  }
 
+  create_resources('ec2api_config', $ec2api_config, $defaults)
+  create_resources('ec2api_api_paste_ini', $api_paste_ini_config, $defaults)
+
+  if $manage_service {
+     $srv_status = 'running'
+  } else {
+     $srv_status = 'stopped'
+  }
+
+  service {'ec2api':
+    ensure => $srv_status,
+    hasrestart => true,
+    require => Package['ec2api']
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,8 +3,8 @@
 # These parameters need to be accessed from several locations and
 # should be considered to be constant
 class ec2api::params {
-  $package_name          = 'openstack-ec2-api'
-  $api_service_name      = 'openstack-ec2-api'
+  $package_name          = 'ec2api'
+  $api_service_name      = 'ec2api'
   $ec2api_config         = '/etc/ec2api/ec2api.conf'
   $ec2api_api_paste_ini  = '/etc/ec2api/api-paste.ini'
   $s3_service_name       = 'openstack-ec2-api-s3'
