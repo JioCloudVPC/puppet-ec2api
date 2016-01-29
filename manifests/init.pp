29dfc532a43b590a89c76ff456f5bf004902676d
vpramo <pramod.venkatesh@ril.com>
2016-01-29 17:32:49 +0530

    Fixed minor issues

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,30 +21,14 @@ class ec2api(
     name   => $ec2api::params::package_name,
   }
 
-  # Chanage onwer, group and permissions to config files
-  file { $ec2api::params::ec2api_config:
-    ensure  => present,
-    owner   => 'ec2api',
-    group   => 'ec2api',
-    mode    => '0644',
-    require => Package['ec2api'],
-  }
-
-  file { $ec2api::params::ec2api_api_paste_ini:
-    ensure  => present,
-    owner   => 'ec2api',
-    group   => 'ec2api',
-    mode    => '0644',
-    require => Package['ec2api'],
-  }
   
   $defaults = {
     notify => Service['ec2api'],
     require => Package['ec2api']
   }
 
-  create_resources('ec2api_config', $ec2api_config, $defaults)
-  create_resources('ec2api_api_paste_ini', $api_paste_ini_config, $defaults)
+  create_resources(ec2api_config, $ec2api_config, $defaults)
+  create_resources(ec2api_api_paste_ini, $api_paste_ini_config, $defaults)
 
   if $manage_service {
      $srv_status = 'running'
